c67c6fe20c2d9ef6358cfc2ac49fbf48d27c6615
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-08-01 21:01:19 +0000

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+    Basic annotations for variables.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+        Returns a concatenated string.
+    '''
+    return str1 + str2
