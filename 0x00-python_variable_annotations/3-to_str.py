5e311b6ff66de3543943d8a4c60a694e2fe1bad1
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-08-01 21:21:14 +0000

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+    Basic annotations for variables.
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+        Returns the floor of the float.
+    '''
+    return str(n)
