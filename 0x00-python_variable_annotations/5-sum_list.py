95b56dc1fd61f5e4172eb2e4a7ecb222d6647b3a
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-08-01 21:34:36 +0000

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import List
+'''
+    Basic annotations for variables.
+'''
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+        Returns sum of floats.
+    '''
+    return sum(input_list)
